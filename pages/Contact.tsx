af7d9fb57d6ff9541bd6a0104e7bc1fcfced1ad7
IKU <IKU@MacBook-Pro.local>
2021-12-30 20:36:02 +0900

    add contact

---
+++ pages/Contact.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Layout from "./layout/Layout";
+import style from "/styles/contact/contact.module.scss"
+
+
+
+const Contact: NextPage = () => {
+  return (
+    <Layout title="Kana's Portfolio| contact" className={ style.contact }>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBUlDxo6zq0qkOn8vxDjpsOWAZ2bHuXBnHNVJzxgMYapZY6Q/viewform?embedded=true">読み込んでいます…</iframe>
+    </Layout>
+  )
+}
+
+export default Contact
